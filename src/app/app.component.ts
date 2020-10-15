2ff3d365948544fdc8b833af35c67a33f36aa61a
Tiffany Ceasor <tceasor@microsoft.com>
2020-10-14 23:58:45 -0700

    Added registration page and sidd menu toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+// create component that is a page with labels on each page
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Home',
+      url: 'home',
+      icon: "home"
+    
+    },
+    {
+      title: 'Register',
+      url: 'register',
+      icon: 'pencil'
+    }
+    
+  ];
+ 
+
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -24,4 +42,5 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+ 
 }
